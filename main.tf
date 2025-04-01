7b65535a7fb354bae468a9b0ad2b8ab4d605b482
Niek van Raaij <nvanraaij@schubergphilis.com>
2025-04-01 09:29:45 +0200

    fix: minor typo

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "this" {
-  count = var.create_new_resource_group ? 0 : 1
+  count = var.create_new_resource_group ? 1 : 0
 
   name     = var.resource_group_name
   location = var.location
